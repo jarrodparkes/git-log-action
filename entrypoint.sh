2af2b34d7b6b8a62fa1edb8a6df7e020fc56dbac
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-05-25 11:09:28 -0500

    update bash typo again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ $1 != '' ]]; then
-    echo "commit: [not-specified]" 
-    log=$(git log --pretty=format:'%h - %s (%an)')
-else   
     echo "commit: $1"      
     log=$(git log master..$1 --pretty=format:'%h - %s (%an)')
+else 
+    echo "commit: [not-specified]" 
+    log=$(git log --pretty=format:'%h - %s (%an)')  
 fi
 
 echo $log
