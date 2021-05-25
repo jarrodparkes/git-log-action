cb290c1d20c2bd3ae80feff975f250e98cec45ec
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-05-25 11:46:15 -0500

    require commit arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
 
-if [ -z "$1" ]; then
-    echo "commit: [not-specified]" 
-    log=$(git log -n 10 develop.. --pretty=format:'%h - %s (%an)')  
-else 
-    echo "commit: $1"      
-    log=$(git log develop..$1 --pretty=format:'%h - %s (%an)')
-fi
+echo "commit: $1"      
+
+log=$(git log develop..$1 --pretty=format:'%h - %s (%an)')
 
 echo "$log"
 echo ::set-output name=log::"$log"
